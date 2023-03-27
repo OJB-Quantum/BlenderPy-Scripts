e593f0f336c1142389832f86d910b417fbc4b00a
Onri Jay Benally <88035770+OJB-Quantum@users.noreply.github.com>
2023-03-27 09:12:28 -0500

    Add files via upload

---
+++ Caffeine Molecule Rendering in Blender.py
@@ -0,0 +1,139 @@
+# %%
+import bpy
+bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (1, 1, 1, 1)
+bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (1, 1, 1, 1)
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.space_data.context = 'RENDER'
+bpy.context.space_data.context = 'VIEW_LAYER'
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.space_data.context = 'WORLD'
+bpy.context.space_data.context = 'DATA'
+bpy.context.space_data.system_bookmarks_active = 2
+bpy.context.space_data.system_bookmarks_active = 3
+bpy.context.space_data.params.filename = "Caffeine in 4K with white bkgnd.png"
+bpy.ops.image.save_as(save_as_render=True, copy=True, filepath="C:\\Users\\ojbsn\\Downloads\\Caffeine in 4K with white bkgnd.png", relative_path=True, show_multiview=False, use_multiview=False)
+bpy.ops.image.save_as(save_as_render=True, copy=True, filepath="C:\\Users\\ojbsn\\Downloads\\Caffeine in 4K with white bkgnd.png", show_multiview=False, use_multiview=False)
+bpy.context.space_data.context = 'WORLD'
+bpy.context.space_data.context = 'VIEW_LAYER'
+bpy.context.space_data.context = 'RENDER'
+bpy.context.space_data.context = 'DATA'
+bpy.context.space_data.context = 'OBJECT'
+bpy.context.space_data.context = 'SCENE'
+bpy.context.space_data.context = 'VIEW_LAYER'
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.scene.render.resolution_x = 7860
+bpy.context.scene.render.resolution_y = 4320
+bpy.context.space_data.context = 'RENDER'
+bpy.context.scene.render.engine = 'BLENDER_EEVEE'
+bpy.context.scene.render.engine = 'CYCLES'
+bpy.context.scene.cycles.device = 'GPU'
+bpy.context.scene.cycles.feature_set = 'EXPERIMENTAL'
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.scene.render.use_border = True
+bpy.context.scene.render.use_border = False
+bpy.context.scene.render.use_border = True
+bpy.context.scene.render.use_border = False
+bpy.context.space_data.context = 'TOOL'
+bpy.context.space_data.context = 'RENDER'
+bpy.ops.outliner.item_activate(deselect_all=True)
+bpy.context.space_data.context = 'MODIFIER'
+bpy.context.space_data.context = 'TOOL'
+bpy.context.space_data.context = 'RENDER'
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.space_data.context = 'VIEW_LAYER'
+bpy.context.space_data.context = 'WORLD'
+bpy.context.space_data.context = 'SCENE'
+bpy.context.space_data.context = 'WORLD'
+bpy.context.space_data.context = 'OBJECT'
+bpy.context.space_data.context = 'PARTICLES'
+bpy.context.space_data.context = 'PHYSICS'
+bpy.context.space_data.context = 'CONSTRAINT'
+bpy.context.space_data.context = 'DATA'
+bpy.context.space_data.context = 'MODIFIER'
+bpy.context.space_data.context = 'MATERIAL'
+bpy.context.object.active_material_index = 2
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.7484, 0.7484, 0.7484, 1)
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.7484, 0.7484, 0.7484, 1)
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.space_data.params.filename = "Caffeine in 8K with white bkgnd.png"
+bpy.context.space_data.system_bookmarks_active = 2
+bpy.context.space_data.system_bookmarks_active = 3
+bpy.context.space_data.system_bookmarks_active = 1
+bpy.context.space_data.system_bookmarks_active = 0
+bpy.context.space_data.system_bookmarks_active = 3
+bpy.ops.image.save_as(save_as_render=True, copy=True, filepath="C:\\Users\\ojbsn\\Downloads\\Caffeine in 8K with white bkgnd.png", show_multiview=False, use_multiview=False)
+bpy.context.space_data.show_region_toolbar = True
+bpy.context.space_data.show_region_ui = True
+bpy.context.space_data.show_region_ui = False
+bpy.ops.outliner.item_activate(deselect_all=True)
+bpy.context.space_data.context = 'OBJECT'
+bpy.context.space_data.context = 'SCENE'
+bpy.context.space_data.context = 'CONSTRAINT'
+bpy.context.space_data.context = 'DATA'
+bpy.context.space_data.context = 'PHYSICS'
+bpy.context.space_data.context = 'PARTICLES'
+bpy.context.space_data.context = 'MODIFIER'
+bpy.context.space_data.context = 'OBJECT'
+bpy.context.space_data.context = 'WORLD'
+bpy.context.space_data.context = 'SCENE'
+bpy.context.space_data.context = 'VIEW_LAYER'
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.space_data.context = 'RENDER'
+bpy.context.space_data.context = 'TOOL'
+bpy.ops.outliner.item_activate(deselect_all=True)
+bpy.ops.outliner.item_rename()
+bpy.ops.outliner.item_activate(deselect_all=True)
+bpy.context.space_data.context = 'TEXTURE'
+bpy.context.space_data.context = 'MATERIAL'
+bpy.context.object.active_material_index = 2
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.507067, 0.507067, 0.507067, 1)
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.507067, 0.507067, 0.507067, 1)
+bpy.context.object.active_material_index = 2
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.911392, 0.911392, 0.911392, 1)
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.527717, 0.527717, 0.527717, 1)
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.54885, 0.54885, 0.54885, 1)
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.184468, 0.184468, 0.184468, 1)
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (1, 1, 1, 1)
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.559599, 0.559599, 0.559599, 1)
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.559599, 0.559599, 0.559599, 1)
+bpy.context.space_data.params.filename = "Adjusted Caffeine in 8K with white bkgnd.png"
+bpy.ops.image.save_as(save_as_render=True, copy=True, filepath="C:\\Users\\ojbsn\\Downloads\\Adjusted Caffeine in 8K with white bkgnd.png", show_multiview=False, use_multiview=False)
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.scene.render.resolution_x = 7680
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.object.shade_smooth()
+bpy.ops.object.shade_smooth(use_auto_smooth=True)
+bpy.data.window_managers["WinMan"].(null) = True
+bpy.ops.object.shade_smooth()
+bpy.ops.object.shade_smooth(use_auto_smooth=True)
+bpy.data.window_managers["WinMan"].(null) = True
+bpy.context.space_data.context = 'MATERIAL'
+bpy.data.materials["Hydrogen"].node_tree.nodes["Principled BSDF"].inputs[9].default_value = 0.1
+bpy.context.object.active_material_index = 3
+bpy.data.materials["Oxygen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.401967, 0, 0.00315694, 1)
+bpy.data.materials["Oxygen"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.409, 0, 0.00321228, 1)
+bpy.context.space_data.context = 'TEXTURE'
+bpy.context.space_data.context = 'MATERIAL'
+bpy.context.space_data.params.filename = "Adjusted Caffeine in 8K with white bkgnd.png"
+bpy.ops.image.save_as(save_as_render=True, copy=True, filepath="C:\\Users\\ojbsn\\Downloads\\Adjusted Caffeine in 8K with white bkgnd.png", show_multiview=False, use_multiview=False)
+bpy.context.space_data.context = 'MATERIAL'
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.space_data.context = 'VIEW_LAYER'
+bpy.context.space_data.context = 'RENDER'
+bpy.context.scene.cycles.feature_set = 'SUPPORTED'
+bpy.context.scene.cycles.samples = 1541
+bpy.context.scene.cycles.samples = 1500
+bpy.context.space_data.context = 'OUTPUT'
+bpy.context.scene.render.resolution_y = 2160
+bpy.context.scene.render.resolution_x = 3840
+bpy.context.space_data.context = 'TOOL'
+bpy.context.space_data.context = 'RENDER'
+bpy.context.space_data.params.filename = "Adjusted Caffeine in 4K with white bkgnd.png"
+bpy.ops.image.save_as(save_as_render=True, copy=True, filepath="C:\\Users\\ojbsn\\Downloads\\Adjusted Caffeine in 4K with white bkgnd.png", show_multiview=False, use_multiview=False)
+bpy.ops.outliner.item_activate(deselect_all=True)
+bpy.ops.object.editmode_toggle()
+bpy.ops.object.editmode_toggle()
+
+
+
+
